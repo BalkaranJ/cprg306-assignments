aec680ed431afff6934763f7f05152f8b82e7b46
Balkaran <balkaranjaswal@gmail.com>
2025-03-11 20:13:26 -0600

    Week 7 update page.js
    using useState to store items dynamically. Implemented handleAddItems to add new items with a unique ID. Passes item props to ItemList to display current items

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,11 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import NewItem from "./new-item";
 import Link from "next/link";
 import ItemList from "./item-list";
+import itemsData from "./items-data";
 
 export default function Page() {
+
+    const [items, setItems] = useState(itemsData);
+
+    //Function to add a new item to the list
+    const handleAddItem = (newItem) => {
+        setItems((prevItems) => [...prevItems, { id: prevItems.length + 1, ...newItem }]);
+    }
+
     return (
-        <main className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
-            <h1 className="text-3xl font-bold text-gray-800 mb-6">Shopping List</h1>
-            <ItemList />
+        <main className="min-h-screen flex flex-col items-center justify-center p-6 bg-gray-100">
+        <h1 className="text-3xl font-bold mb-6">Shopping List</h1>
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={items} />
         </main>
-    );
+      );
 }
